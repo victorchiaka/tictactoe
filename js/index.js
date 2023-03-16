eda1e3dcae5360842cc48c5aa41011764e27e239
code-alchemist205 <chiakavictor205@gmail.com>
2023-03-16 20:36:31 +0100

    can now check for winnings, disables the button on click and on winning, displays scores, restarting with restart game button possible, changed the button div to just button, made some cleanups

--- js/index.js
+++ js/index.js
@@ -1,51 +1,100 @@
 const WINNING_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
-
-let displayInfo = document.getElementById("display-info");
-
-displayInfo.innerHTML = "your turn";
 
 const buttons = document.querySelectorAll(".box");
 
-/**
- * This function is supposed to let the computer play for itself,
- * after the player have picked a box
- */
-function opponentPlay() {
-    let position = Math.floor(Math.random() * 9);
-    if (buttons[position].textContent === "") {
-        displayInfo.innerHTML = "O's turn";
-        buttons[position].innerHTML = "O";
-        displayInfo.innerHTML = "your turn";
-    } else {
-        opponentPlay();
-    }
+let x_scores = document.getElementById("x_scores");
+let o_scores = document.getElementById("o_scores");
+let displayInfo = document.getElementById("display-info");
+let x_winnings = 0;
+let o_winnings = 0;
 
+displayInfo.innerHTML = "your turn";
+
+x_scores.textContent = "X: " + x_winnings;
+o_scores.textContent = "O: " + o_winnings;
+
+function opponentPlay() {
+  let position = Math.floor(Math.random() * 9);
+  if (buttons[position].textContent === "") {
+    buttons[position].innerHTML = "O";
+    buttons[position].disabled = true;
+    displayInfo.innerHTML = "your turn";
+    if (isWinning()) {
+      // Increments O scores
+      o_winnings++;
+      o_scores.innerHTML = "O: " + o_winnings;
+      setTimeout(() => {
+          for (let remainingButtons of buttons) {
+            if (remainingButtons.innerHTML === "") {
+              remainingButtons.disabled = true;
+            }
+          }
+        },
+      1000
+      );
+      alert("O won"); // remove this later
+    }
+  } else {
+    opponentPlay();
+  }
 }
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        if (button.textContent === "") {
-            button.innerHTML = "X";
-            opponentPlay();
-        } else {
-            displayInfo = "your turn";
-        }
-    });
+  button.addEventListener("click", () => {
+    if (button.textContent === "") {
+      button.innerHTML = "X";
+      button.disabled = true;
+      if (isWinning()) {
+        // Increments X scores
+        x_winnings++;
+        x_scores.innerHTML = "X: " + x_winnings;
+        setTimeout(() => {
+            for (let remainingButtons of buttons) {
+              if (remainingButtons.innerHTML === "") {
+                remainingButtons.disabled = true;
+              }
+            }
+          },
+          1000
+        );
+        alert("X won"); // remove this later
+      } else {
+        displayInfo.innerHTML = "O's turn";
+        setTimeout(
+          opponentPlay, 
+          1000
+        );
+      }
+    } else {
+    }
+  });
 });
 
 function restartGame() {
-    buttons.forEach((button) => {
-        button.innerHTML = "";
-    });
+  buttons.forEach((button) => {
+    button.innerHTML = "";
+    button.disabled = false;
+  });
 }
 
-// right a function that get's the position of each button of first x and secondly o, then pass it to an array and match it with the WINNING_COMBINATIONS array and see if that pattern exist in the in array, if so then echo the winner as maybe an alert(), then fixe it well and improve the logic and ui
+function isWinning() {
+  for (let COMBOS of WINNING_COMBINATIONS) {
+    if (
+      buttons[COMBOS[0]].textContent === buttons[COMBOS[1]].textContent &&
+      buttons[COMBOS[1]].textContent === buttons[COMBOS[2]].textContent &&
+      buttons[COMBOS[0]].textContent != ""
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
